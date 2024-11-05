382766075580f0db797f96b6a583d7a11f50eaf9
Soumya Ranjan Bhuyan <sbhuyan@massivelyparallel.com>
2024-11-05 16:52:36 +0000

    Gather time by calling thread manager

--- analyze.sh
+++ analyze.sh
@@ -200,7 +200,8 @@ for i in ${core[@]}
 do
   # time
   start=`date +%s.%N`;\
-  ./$algo $iva_data $iva_data $i;\
+#  ./$algo $iva_data $iva_data $i;\
+curl -D - --header "Content-Type: application/json" --output - --request POST --data '{"id": 1, "lib":"libmm.so", "fn": "main", "row": '\""$iva_data"\"', "core": '\""$i"\"'}' 192.168.1.36:8092/run
   end=`date +%s.%N`;\
   time_parallel+=(`printf '%.8f' $( echo "$end - $start" | bc -l )`);
 
